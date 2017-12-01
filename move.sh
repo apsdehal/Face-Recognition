a249bf94400a177f328faef91d5ffbf34554a0e1
Amanpreet Singh <apsdehal@gmail.com>
2017-12-01 06:58:41 -0500

    Add move shell file for easy moving

---
+++ move.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd $1
+mkdir 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+
+mv subject01* 1
+mv subject02* 2
+mv subject03* 3
+mv subject04* 4
+mv subject05* 5
+mv subject06* 6
+mv subject07* 7
+mv subject08* 8
+mv subject09* 9
+mv subject10* 10
+mv subject11* 11
+mv subject12* 12
+mv subject13* 13
+mv subject14* 14
+mv subject15* 15
+
+rm Readme.txt
